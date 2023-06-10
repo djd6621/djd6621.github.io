1877a0fe2c05e53f5e56ca41d2b3c2d6543cda22
djd6621 <djd6621@rit.edu>
2023-06-09 20:12:49 -0400

    build: :construction_worker: Config files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,8 +2,21 @@
 export default {
 	content: ["./index.html", "./src/**/*.{html,js,svelte,ts}"],
 	theme: {
-		extend: {},
+		extend: {
+			flex: {
+				'2': '2',
+				'3': '3',
+				'4': '4',
+				'5': '5',
+				'6': '6',
+				'7': '7',
+				'8': '8',
+				'9': '9',
+				'10': '10',
+				'11': '11'
+			}
+		},
 	},
-	plugins: [],
+	plugins: [require("daisyui")],
 };
 
