e1193604f2675b5208a188cbf6f1ae7ff994ec24
djd6621 <djd6621@rit.edu>
2023-06-09 20:12:18 -0400

    refactor: :fire: Removed boilerplate and added init

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+
+</script>
+
+<div class="flex flex-1 mx-4 justify-between">
+    <h1 class="flex uppercase items-center">
+        DarekDunbar
+    </h1>
+    <div class="flex gap-20">
+        <div class="flex">
+            <button>/education</button>
+        </div>
+        <div class="flex">
+            <button>/work</button>
+        </div>
+        <div class="flex">
+            <button>/contact</button>
+        </div>
+    </div>
+    <div class="flex avatar p-2">
+        <div class="w-16 rounded-full">
+            <img src="./profile.JPG" alt=""/>
+        </div>
+    </div>
+</div>
